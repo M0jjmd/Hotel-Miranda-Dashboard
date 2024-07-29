d03d3b917755f409da8f3f0632a3484c756de50a
M0jjmd <moamine517@gmail.com>
2024-07-29 17:56:55 +0200

    created sidebar and header and give some styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import './sidebar.css'
 
-function sidebar() {
+const Sidebar = () => {
   return (
-    <div>sidebar</div>
+    <>
+      <aside className="sidebar">
+        <ul>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/bookings">Bookings</Link></li>
+          <li><Link to="/contacts">Rooms</Link></li>
+          <li><Link to="/rooms">Contact</Link></li>
+          <li><Link to="/users">Users</Link></li>
+        </ul>
+      </aside>
+    </>
   )
 }
 
-export default sidebar
+export default Sidebar
