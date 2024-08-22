25bd4663bf430fc35d31fa76d0ea5f38194d5e61
M0jjmd <moamine517@gmail.com>
2024-08-22 22:13:55 +0200

    Finished Edit, delete and create new user

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as S from '../styles/tablesForm'
+import { useDispatch, useSelector } from 'react-redux'
+import { GetUsers } from '../features/users/usersThunk'
+import { useAuth } from '../context/AuthContext'
+import EditableRow from './users/EditableRow'
+import AddUsers from './users/AddUsers'
 
-function Employees({ employees }) {
+function Employees() {
   const [filter, setFilter] = useState('ALL')
   const [searchTerm, setSearchTerm] = useState('')
-  const [isFormOpen, setIsFormOpen] = useState(false)
+  // const [isFormOpen, setIsFormOpen] = useState(false)
+  const { state, dispatch } = useAuth()
+
+  const dipatchRooms = useDispatch()
+
+  const users = useSelector((state) => state.users.data)
+  const usersStatus = useSelector((state) => state.users.status)
+
+  useEffect(() => {
+    if (usersStatus === 'idle') {
+      dipatchRooms(GetUsers())
+      dispatch({ type: 'CLOSE_FORM' })
+    }
+  }, [dipatchRooms, usersStatus])
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter)
@@ -14,19 +32,29 @@ function Employees({ employees }) {
     setSearchTerm(e.target.value.toLowerCase());
   }
 
-  const filteredEmployees = employees
-    .filter(employee => {
-      if (filter !== 'ALL' && employee.Estado !== filter) return false
-      const combinedString = JSON.stringify(employee).toLowerCase()
+  // const filteredEmployees = users
+  //   .filter(user => {
+  //     if (filter !== 'ALL' && user.Estado !== filter) return false
+  //     const combinedString = JSON.stringify(user).toLowerCase()
+  //     return combinedString.includes(searchTerm)
+  //   })
+  const filteredUsers = users
+    .filter(user => {
+      if (filter !== 'ALL' && user.State !== filter) return false
+      const combinedString = JSON.stringify(user).toLowerCase()
       return combinedString.includes(searchTerm)
     })
 
-  const handleFormToggle = () => {
-    setIsFormOpen(!isFormOpen)
-  }
+  // const handleFormToggle = () => {
+  //   setIsFormOpen(!isFormOpen)
+  // }
 
-  const handleAddUser = () => {
-    setIsFormOpen(false)
+  const handleFormToggle = () => {
+    if (state.isFormOpen) {
+      dispatch({ type: 'CLOSE_FORM' });
+    } else {
+      dispatch({ type: 'OPEN_FORM' });
+    }
   }
 
   return (
@@ -41,43 +69,16 @@ function Employees({ employees }) {
         <S.Button active={filter === 'ALL'} onClick={() => handleFilterChange('ALL')}>All Employees</S.Button>
         <S.Button active={filter === 'ACTIVE'} onClick={() => handleFilterChange('ACTIVE')}>Active Employees</S.Button>
         <S.Button active={filter === 'INACTIVE'} onClick={() => handleFilterChange('INACTIVE')}>Inactive Employees</S.Button>
-        <S.AddButton onClick={handleFormToggle}>Add Booking</S.AddButton>
+        <S.AddButton onClick={handleFormToggle}>Add User</S.AddButton>
       </S.FilterContainer>
 
-      <S.FormContainer open={isFormOpen}>
-        <label>Full Name</label>
-        <S.Input type="text" placeholder="Enter full name" />
-
-        <label>Position</label>
-        <S.Select>
-          <option value="Manager">Manager</option>
-          <option value="Reception">Reception</option>
-          <option value="Room Service">Room Service</option>
-        </S.Select>
-
-        <label>Email</label>
-        <S.Input type="email" placeholder="Enter email" />
-
-        <label>Phone Number</label>
-        <S.Input type="tel" placeholder="Enter phone number" />
-
-        <label>Start Date</label>
-        <S.Input type="date" />
-
-        <label>Job Description</label>
-        <S.TextArea placeholder="Enter job description" />
-
-        <label>Status</label>
-        <S.ToggleButton active={true}>Active</S.ToggleButton>
-
-        <label>Password</label>
-        <S.Input type="password" placeholder="Enter password" />
-        <S.Button onClick={handleAddUser}>Add Booking</S.Button>
-      </S.FormContainer>
-
-      {!isFormOpen && (
+      {state.isFormOpen ? (
         <>
-          {filteredEmployees.length > 0 ? (
+          <AddUsers />
+        </>
+      ) : (
+        <>
+          {filteredUsers.length > 0 ? (
             <S.Table>
               <S.TableHeader>
                 <tr>
@@ -86,22 +87,11 @@ function Employees({ employees }) {
                   <S.HeaderCell>Schedule</S.HeaderCell>
                   <S.HeaderCell>Contact</S.HeaderCell>
                   <S.HeaderCell>Status</S.HeaderCell>
+                  <S.HeaderCell>Actions</S.HeaderCell>
                 </tr>
               </S.TableHeader>
-              <S.TableBody>
-                {filteredEmployees.map(employee => (
-                  <S.TableRow key={employee.IDdeEmpleado}>
-                    <S.TableCell>
-                      <S.TablePhoto src={employee.Foto} alt={employee.NombreCompleto} />
-                      {employee.NombreCompleto}
-                    </S.TableCell>
-                    <S.TableCell>{employee.DescripciónDelPuesto}</S.TableCell>
-                    <S.TableCell>{employee.FechaDeAlta}</S.TableCell>
-                    <S.TableCell>{employee.Teléfono}</S.TableCell>
-                    <S.TableCell>{employee.Estado}</S.TableCell>
-                  </S.TableRow>
-                ))}
-              </S.TableBody>
+
+              <EditableRow filteredUsers={filteredUsers} />
             </S.Table>
           ) : (
             <S.NoResults>No search results found</S.NoResults>
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react"
 import Employess from '../components/Employees.jsx'
 
 const Users = () => {
-    const [employees, setEmployees] = useState([])
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        fetch('../employees.json')
-            .then(response => response.json())
-            .then(data => {
-                setEmployees(data)
-                setLoading(false)
-            })
-            .catch(error => {
-                console.error('Error fetching employee data:', error)
-                setLoading(false)
-            })
-    }, [])
-
-    if (loading) {
-        return <div>Loading...</div>
-    }
-
     return (
         <>
             <div>
-                <Employess employees={employees} />
+                <Employess />
             </div>
         </>
     )
