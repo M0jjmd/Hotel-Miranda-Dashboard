0bc9f8fdb96f6bef547895411bb89a60378e4bbe
M0jjmd <moamine517@gmail.com>
2024-11-07 17:22:25 +0100

    changed cypress url to s3 dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://hotelmirandadashboard.s3-website.eu-north-1.amazonaws.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
