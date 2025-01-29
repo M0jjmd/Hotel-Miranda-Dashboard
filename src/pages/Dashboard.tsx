c39dbec8bca8cbb2cdafc99c0dac64c80a677b37
M0jjmd <moamine517@gmail.com>
2025-01-29 16:02:59 +0100

    Changes on dashboard view

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,16 +2,44 @@ import DashboardStats from '../components/DashboardStats'
 import InteractiveCalendar from '../components/InteractiveCalendar'
 import LatestMessages from '../components/LatestMessages'
 import * as S from '../styles/dashboardStyles'
+import styled from 'styled-components'
+
+const DashboardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+const TopSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 2rem;
+`
+
+const FullWidthSection = styled.div`
+  width: 100%;
+`
 
 const Dashboard = () => {
     return (
-        <>
-            <S.DashboardContent>
+        // <>
+        //     <S.DashboardContent>
+        //         <DashboardStats />
+        //         <InteractiveCalendar />
+        //         <LatestMessages />
+        //     </S.DashboardContent>
+        // </>
+        <DashboardContent>
+            <TopSection>
                 <DashboardStats />
                 <InteractiveCalendar />
+            </TopSection>
+            <FullWidthSection>
                 <LatestMessages />
-            </S.DashboardContent>
-        </>
+            </FullWidthSection>
+        </DashboardContent>
     )
 }
 
