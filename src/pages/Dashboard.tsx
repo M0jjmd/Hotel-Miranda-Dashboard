f831b61c45ee69205c9d69e0f389174c18d390eb
M0jjmd <moamine517@gmail.com>
2025-02-10 23:54:37 +0100

    Changes on dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
+import * as S from '../styles/dashboardStyles'
 import DashboardStats from '../components/DashboardStats'
 import InteractiveCalendar from '../components/InteractiveCalendar'
 import LatestMessages from '../components/LatestMessages'
-import * as S from '../styles/dashboardStyles'
 
 const Dashboard = () => {
     return (
