50dfdbba3d7351d63acd0a0c3eb24f44803709fd
M0jjmd <moamine517@gmail.com>
2025-02-25 16:42:12 +0100

    Deleted more comments

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://hotelmirandadashboard.s3-website.eu-north-1.amazonaws.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
 
   },
